885a2b8c5ba28781d1c7b604be70c6e9eb989a4a
Jaewon Yang <neverdup@gmail.com>
2024-11-25 23:55:21 +0900

    move content of database.py to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,41 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import pytest
+from fastapi.testclient import TestClient
+from main import app
+from database import get_db, Base
+
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@localhost:15432/bulletin-board"
+
+
+engine = create_engine(
+    "postgresql://postgres:mysecretpassword@localhost:15432/bulletin-board-test"
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture()
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture()
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
 
 
 @pytest.fixture
--- tests/database.py
+++ tests/database.py
@@ -1,38 +1,38 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import pytest
-from fastapi.testclient import TestClient
-from main import app
-from database import get_db, Base
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# import pytest
+# from fastapi.testclient import TestClient
+# from main import app
+# from database import get_db, Base
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@localhost:15432/bulletin-board"
-
-
-engine = create_engine(
-    "postgresql://postgres:mysecretpassword@localhost:15432/bulletin-board-test"
-)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@localhost:15432/bulletin-board"
 
 
-@pytest.fixture()
-def session():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# engine = create_engine(
+#     "postgresql://postgres:mysecretpassword@localhost:15432/bulletin-board-test"
+# )
+
+# TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-@pytest.fixture()
-def client(session):
-    def override_get_db():
-        try:
-            yield session
-        finally:
-            session.close()
+# @pytest.fixture()
+# def session():
+#     Base.metadata.drop_all(bind=engine)
+#     Base.metadata.create_all(bind=engine)
+#     db = TestingSessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
 
-    app.dependency_overrides[get_db] = override_get_db
-    yield TestClient(app)
+
+# @pytest.fixture()
+# def client(session):
+#     def override_get_db():
+#         try:
+#             yield session
+#         finally:
+#             session.close()
+
+#     app.dependency_overrides[get_db] = override_get_db
+#     yield TestClient(app)
